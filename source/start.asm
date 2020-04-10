c2975115c4153211dc039891975b20ffe444a309
mintsuki <mintsuki@protonmail.com>
2020-04-10 16:32:02 +0200

    start.asm: Request video mode

--- source/start.asm
+++ source/start.asm
@@ -2,7 +2,7 @@ section .stivalehdr
 
 header:
     dq stack.top ; rsp
-    dw 0         ; video mode
+    dw 1         ; video mode
     dw 0         ; fb_width, 0 is default
     dw 0         ; fb_height, ditto
     dw 0         ; fb_bpp, ditto
